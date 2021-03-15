efce71c15cbdcb62808f0343614e0dfe93e207d0
Amozik <amozik@rambler.ru>
2021-03-15 21:03:40 +0200

    add bullet with Builder (hack)

--- Assets/Scripts/General/Player/PlayerWeapon.cs
+++ Assets/Scripts/General/Player/PlayerWeapon.cs
@@ -6,27 +6,26 @@ namespace General.Player
     internal class PlayerWeapon : IWeapon
     {
         private Transform _bulletSpawner;
-        private Rigidbody2D _bullet;
+        private GameObject _bullet;
         private float _force;
         private ViewServices _bulletPool;
 
-        public PlayerWeapon(Transform bulletSpawner, Rigidbody2D bullet, float force)
+        public PlayerWeapon(Transform bulletSpawner, Rigidbody2D bullet, Sprite bulletSprite, float force)
         {
             _bulletSpawner = bulletSpawner;
-            _bullet = bullet;
             _force = force;
             _bulletPool = new ViewServices();
+            _bullet = Bullet.CreateBullet(bulletSprite);
+            _bullet.SetActive(false);
         }
         
         public void Shoot()
         {
-            var temAmmunition = _bulletPool.Create(_bullet.gameObject);
+            var temAmmunition = _bulletPool.Create(_bullet);
             temAmmunition.transform.position = _bulletSpawner.position;
             
-            //var temAmmunition = Object.Instantiate(_bullet, _bulletSpawner.position, _bulletSpawner.rotation);
             temAmmunition.GetComponent<Rigidbody2D>().AddForce(_bulletSpawner.up * _force);
-            //temAmmunition.GetComponent<Bullet>().OnBecameInvisibleEvent += OnBulletOutOfCamera;
-            temAmmunition.GetComponent<Bullet>().OnBecameInvisibleEvent += gameObject => _bulletPool.Destroy(_bullet.gameObject, gameObject);
+            temAmmunition.GetComponent<Bullet>().OnBecameInvisibleEvent += gameObject => _bulletPool.Destroy(_bullet, gameObject);
         }
     }
 }
